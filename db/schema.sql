0496e21dcf3059f446f7d02d0c326d206b7e9d00
Jared Elliott <bootcamp@jaredclt.com>
2024-01-23 15:30:25 -0500

    Created .sql files. Completed SCHEMA.SQL, added notes to SEED.sql

---
+++ db/schema.sql
@@ -0,0 +1,46 @@
+-- Creates the database & table structure that is needed
+-- -- -- -- -- -- -- -- -- --
+-- Department
+-- | id *
+-- | name
+-- Role
+-- | id **
+-- | title
+-- | salary
+-- | department_id *
+-- Employee
+-- | id
+-- | first_name
+-- | last_name
+-- | role_id **
+-- | manager_id
+-- -- -- -- -- -- -- -- -- --
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE IF NOT EXISTS employees;
+
+USE employees;
+
+CREATE TABLE department (
+    id      int         NOT NULL    AUTO_INCREMENT,
+    name    VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+    id              INT         NOT NULL    AUTO_INCREMENT,
+    title           VARCHAR(3)  NOT NULL,
+    salary          DECIMAL     NOT NULL,
+    department_id   INT         NOT NULL,
+    FOREIGN KEY(department_id)  REFERENCES department(id)   ON DELETE CASCADE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employee (
+    id          INT         NOT NULL    AUTO_INCREMENT,
+    first_name  VARCHAR(30) NOT NULL,
+    last_name   VARCHAR(30) NOT NULL,
+    role_id     INT         NOT NULL,
+    manager_id  INT         NOT NULL,
+    FOREIGN KEY(role_id)    REFERENCES role(id) ON DELETE CASCADE,
+    PRIMARY KEY(id)
+);
---
+++ db/seed.sql
@@ -0,0 +1,1 @@
+-- Data to pre-load database will be entered here
