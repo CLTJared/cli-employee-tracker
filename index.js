04f1cf43d251c4b6112bb01568a431668a30c55a
Jared Elliott <bootcamp@jaredclt.com>
2024-01-23 22:52:38 -0500

    App start and installation.

---
+++ index.js
@@ -0,0 +1,7 @@
+const CLI = require('./lib/cli.js');
+const cli = new CLI();
+
+/* ================================
+    Run our interface for the user 
+   ================================ */
+cli.startApp();
